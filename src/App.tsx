219f48dc2c1f18c7543c2f1cddad224a515769a7
Duy Phuong Nguyen <mngduyphuong@gmail.com>
2024-06-12 22:55:11 +1000

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle></ModeToggle>
+    </ThemeProvider>
+  );
+}
+
+export default App;
